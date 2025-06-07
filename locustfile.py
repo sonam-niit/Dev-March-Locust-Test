5e1620550b0f941216223aaa12df3ecba458f940
sonam-niit <sonam.gravity@gmail.com>
2025-06-07 12:01:38 +0530

    post test

--- locustfile.py
+++ locustfile.py
@@ -6,3 +6,10 @@ class MyStressTestUser(HttpUser):
     @task
     def get_posts(self):
         self.client.get('/posts')
+      
+    @task  
+    def create_post(self):
+        self.client.post("/posts",json=
+                        {"title":"sonamtest",
+                         "body":"testbody",
+                         "userId":1})
